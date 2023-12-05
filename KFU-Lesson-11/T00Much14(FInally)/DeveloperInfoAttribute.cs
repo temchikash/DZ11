abc43e0042f7f6420ac297120e02cffa33aac980
temchikash <144339324+temchikash@users.noreply.github.com>
2023-12-05 23:23:19 +0300

    Add files via upload

---
+++ KFU-Lesson-11/T00Much14(FInally)/DeveloperInfoAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork11
+{
+    internal class DeveloperInfoAttribute : System.Attribute
+    {
+        public string Name { get; }
+        public string datereliz { get; }
+
+        public DeveloperInfoAttribute() { }
+        public DeveloperInfoAttribute(string name, string datereliz)
+        {
+            Name = name;
+            this.datereliz = datereliz;
+        }
+    }
+}
